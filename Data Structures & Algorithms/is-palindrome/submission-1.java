1a9e229f65cf1ad1eefc8bf3d8538cbf327365b8
PARANJOTHI R <paranjothi8607@gmail.com>
2026-06-23 10:23:31 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        s=s.replaceAll("[^0-9a-zA-Z]","");
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;j--;
+        }
+        return true;
+    }
+}
