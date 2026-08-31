15aa33bbd395499da302f123f2d51ccec97a0895
PARANJOTHI R <paranjothi8607@gmail.com>
2026-08-31 11:23:20 +0530

    17 ms (8.86%) Easy .java

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> hm  = new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            hm.put(nums1[i],1);
+        }
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums2.length;i++){
+            if(hm.containsKey(nums2[i])) min=Math.min(min,nums2[i]);
+        }
+        return min==Integer.MAX_VALUE?-1:min;
+    }
+}
